4bd785f0ebae0070c59025f3866b19b3b86c737a
Polikkox <damian.targosz11@gmail.com>
2019-01-24 13:56:39 +0100

    Add base version of server

--- src/main/java/com/codecool/networking/modes/Client.java
+++ src/main/java/com/codecool/networking/modes/Client.java
@@ -1,4 +1,25 @@
 package com.codecool.networking.modes;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Client {
+
+
+    public void runServer(int portNumber){
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
