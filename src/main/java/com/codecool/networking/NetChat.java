b81dc4ceec4b57509489a774ce6b98edfe502772
Polikkox <damian.targosz11@gmail.com>
2019-01-24 14:16:27 +0100

    Add client socket

--- src/main/java/com/codecool/networking/NetChat.java
+++ src/main/java/com/codecool/networking/NetChat.java
@@ -3,5 +3,6 @@ package com.codecool.networking;
 public class NetChat {
     public static void main(String[] args) {
 
+
     }
 }
--- src/main/java/com/codecool/networking/modes/Client.java
+++ src/main/java/com/codecool/networking/modes/Client.java
@@ -1,10 +1,49 @@
 package com.codecool.networking.modes;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
 
+    private final String HOSTNAME = "localhost";
 
-    public void runClientSocket(){
+    public void runClientSocket(int portNumber){
+
+        try (
+                Socket kkSocket = new Socket(HOSTNAME, portNumber);
+                PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(kkSocket.getInputStream()));
+        ) {
+            BufferedReader stdIn =
+                    new BufferedReader(new InputStreamReader(System.in));
+            String fromServer;
+            String fromUser;
+
+            while ((fromServer = in.readLine()) != null) {
+                System.out.println("Server: " + fromServer);
+                if (fromServer.equals("Bye."))
+                    break;
+
+                fromUser = stdIn.readLine();
+                if (fromUser != null) {
+                    System.out.println("Client: " + fromUser);
+                    out.println(fromUser);
+                }
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + HOSTNAME);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                    HOSTNAME);
+            System.exit(1);
+        }
 
     }
 }
