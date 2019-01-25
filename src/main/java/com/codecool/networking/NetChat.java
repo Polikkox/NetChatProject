19dd9de0ff7f54cccc1c6e6d032ae075b943bc2a
Polikkox <damian.targosz11@gmail.com>
2019-01-25 09:10:06 +0100

    Update main class

--- src/main/java/com/codecool/networking/NetChat.java
+++ src/main/java/com/codecool/networking/NetChat.java
@@ -1,8 +1,32 @@
 package com.codecool.networking;
 
+import com.codecool.networking.modes.Client;
+import com.codecool.networking.modes.Server;
+import com.codecool.networking.view.BuiltMessages;
+
 public class NetChat {
+    final private static int MODE = 0;
+    final private static int HOSTNAME = 1;
+    final private static int PORTNUMBER = 2;
+
     public static void main(String[] args) {
 
+        if(args.length < 3){
+            BuiltMessages.failedArgsMessage();
+            System.exit(1);
+        }
+        int port = Integer.valueOf(args[PORTNUMBER]);
+
+        if(args[MODE].equals("server")){
+            new Server().runServer(port);
+        }
+        else if(args[MODE].equals("client")){
+            new Client().runClientSocket(args[HOSTNAME], port);
+        }
+        else{
+            BuiltMessages.failedModeArgsMessage();
+            System.exit(1);
+        }
 
     }
 }
