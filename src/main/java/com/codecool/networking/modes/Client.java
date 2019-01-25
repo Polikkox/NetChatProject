2d5b17bdf7b0610e0d704c4bad73bc5d0df6cfb7
Polikkox <damian.targosz11@gmail.com>
2019-01-25 09:09:00 +0100

    Update client socket

--- src/main/java/com/codecool/networking/modes/Client.java
+++ src/main/java/com/codecool/networking/modes/Client.java
@@ -1,49 +1,45 @@
 package com.codecool.networking.modes;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
 
-    private final String HOSTNAME = "localhost";
+    private String name;
 
-    public void runClientSocket(int portNumber){
-
-        try (
-                Socket kkSocket = new Socket(HOSTNAME, portNumber);
-                PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(kkSocket.getInputStream()));
-        ) {
-            BufferedReader stdIn =
-                    new BufferedReader(new InputStreamReader(System.in));
-            String fromServer;
-            String fromUser;
-
-            while ((fromServer = in.readLine()) != null) {
-                System.out.println("Server: " + fromServer);
-                if (fromServer.equals("Bye."))
-                    break;
-
-                fromUser = stdIn.readLine();
-                if (fromUser != null) {
-                    System.out.println("Client: " + fromUser);
-                    out.println(fromUser);
-                }
-            }
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + HOSTNAME);
-            System.exit(1);
+    public void runClientSocket(String hostname, int portNumber){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello user enter your name: ");
+        this.name = scanner.nextLine();
+        Socket socket = null;
+        PrintWriter out = null;
+        String in = "";
+        try {
+            socket = new Socket(hostname, portNumber);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream())).readLine();
         } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " +
-                    HOSTNAME);
-            System.exit(1);
+            e.printStackTrace();
         }
+        out.println(name);
+        System.out.println(in);
 
+
+        while (true) {
+            System.out.println("\nEnter a string to send to the server (empty to quit):");
+            String message = scanner.nextLine();
+            if (message == null || message.isEmpty()) {
+                break;
+            }
+            if(name == null){
+                name = message;
+            }
+            out.println(name + ": " + message);
+            System.out.println(in);
+        }
     }
 }
