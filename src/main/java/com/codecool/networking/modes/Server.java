c0d897f9fef9d510f11561d8ccfe8ae0f04302e0
Polikkox <damian.targosz11@gmail.com>
2019-01-25 09:09:15 +0100

    Update Server

--- src/main/java/com/codecool/networking/modes/Server.java
+++ src/main/java/com/codecool/networking/modes/Server.java
@@ -1,35 +1,21 @@
 package com.codecool.networking.modes;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Server {
 
-
     public void runServer(int portNumber){
+        System.out.println("The server is running.");
+        int clientNumber = 1;
+        try (ServerSocket listener = new ServerSocket(portNumber)) {
+            while (true) {
 
-        try (
-                ServerSocket serverSocket = new ServerSocket(portNumber);
-                Socket clientSocket = serverSocket.accept();
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        ) {
-            String inputLine;
-            String outputLine;
+                new ServerThread(listener.accept(), clientNumber++).start();
 
-            while ((inputLine = in.readLine()) != null) {
-                outputLine = "I'm in while loop";
-                out.println(outputLine);
             }
-        }
-        catch (IOException e) {
-            System.out.println("Exception caught when trying to listen on port "
-                    + portNumber + " or listening for a connection");
-            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
