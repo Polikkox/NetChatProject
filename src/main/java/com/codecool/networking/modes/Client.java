b58b89481a7cbb119b31ffd4c3808f4d08242c83
Polikkox <damian.targosz11@gmail.com>
2019-01-25 12:26:04 +0100

    Refactoring

--- src/main/java/com/codecool/networking/modes/Client.java
+++ src/main/java/com/codecool/networking/modes/Client.java
@@ -8,38 +8,66 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-
-    private String name;
+    private Scanner scanner = new Scanner(System.in);
 
     public void runClientSocket(String hostname, int portNumber){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello user enter your name: ");
-        this.name = scanner.nextLine();
-        Socket socket = null;
-        PrintWriter out = null;
-        String in = "";
+
+
+        String name = getName();
+        PrintWriter outcomeMessage = null;
+        BufferedReader incomeMessage = null;
         try {
-            socket = new Socket(hostname, portNumber);
-            out = new PrintWriter(socket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream())).readLine();
-        } catch (IOException e) {
+            Socket socket = new Socket(hostname, portNumber);
+            outcomeMessage = new PrintWriter(socket.getOutputStream(), true);
+            incomeMessage = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
-        out.println(name);
-        System.out.println(in);
+
+        sentNameOfClientToServer(outcomeMessage, name);
+        displayIncomeMessage(incomeMessage);
 
 
         while (true) {
-            System.out.println("\nEnter a string to send to the server (empty to quit):");
-            String message = scanner.nextLine();
-            if (message == null || message.isEmpty()) {
+
+            String userInput = getUserInput();
+            if(checkIfUserWantToDisconnect(userInput)){
                 break;
             }
-            if(name == null){
-                name = message;
-            }
-            out.println(name + ": " + message);
-            System.out.println(in);
+
+
+            outcomeMessage.println(name + ": " + userInput);
+            displayIncomeMessage(incomeMessage);
         }
+    }
+    private void sentNameOfClientToServer(PrintWriter outcomeMessage, String name){
+        outcomeMessage.println(name);
+    }
+    private void displayIncomeMessage(BufferedReader incomeMessage){
+        try {
+            String inputMessage = incomeMessage.readLine();
+            System.out.println(inputMessage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean checkIfUserWantToDisconnect(String message) {
+        if (message == null || message.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    private String getUserInput() {
+        System.out.println("\nEnter a string to send to the server (empty to quit):");
+        return scanner.nextLine();
+
+    }
+
+    private String getName() {
+        System.out.println("Hello user enter your name: ");
+        return scanner.nextLine();
     }
 }
