da0c6aa8a3931bd96b6d8a7f8de8f8da70845738
Polikkox <damian.targosz11@gmail.com>
2019-01-25 09:10:31 +0100

    Add class with already created messages

---
+++ src/main/java/com/codecool/networking/view/BuiltMessages.java
@@ -0,0 +1,24 @@
+package com.codecool.networking.view;
+
+import java.net.Socket;
+
+public class BuiltMessages {
+
+    public static void failedArgsMessage(){
+        System.out.println("Provide correct args to console\n" +
+                "1. Mode = [client/server]\n" +
+                "2. Hostname\n" +
+                "3. Port number\n" +
+                "Example: server localhost 8020");
+    }
+
+    public static void failedModeArgsMessage(){
+        System.out.println("Incorrect mode type chose [server] or [client]");
+    }
+    public static void newUserLogMessage(Socket socket, int clientNumber){
+        System.out.println("New client #" + clientNumber + " connected at " + socket);
+    }
+    public static void setUserNameMessage(int clientNumber, String name){
+        System.out.println("User number: " + clientNumber + " set his nick name to: " + name);
+    }
+}
