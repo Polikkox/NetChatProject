87d3bc769cbe79b22b0b8eacb8e9e0102db69442
Polikkox <damian.targosz11@gmail.com>
2019-01-26 15:54:36 +0100

    Add users to map with Name as key

--- src/main/java/com/codecool/networking/modes/Server.java
+++ src/main/java/com/codecool/networking/modes/Server.java
@@ -1,9 +1,16 @@
 package com.codecool.networking.modes;
 
+import javax.xml.soap.SAAJResult;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Server {
+
+    private Map<String, ServerThread> map = new HashMap<>();
+    private ServerThread serverThread;
 
     public void runServer(int portNumber){
         System.out.println("The server is running.");
@@ -11,11 +18,30 @@ public class Server {
         try (ServerSocket listener = new ServerSocket(portNumber)) {
             while (true) {
 
-                new ServerThread(listener.accept(), clientNumber++).start();
-
+                serverThread = new ServerThread(listener.accept(), clientNumber++, this);
+                serverThread.start();
             }
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    public Socket getSocket(String clientID){
+        return map.get(clientID).getSocket();
+    }
+    public String getOnlineUsers(){
+        StringBuilder sb = new StringBuilder("Users online:\n");
+
+        this.map.entrySet().stream()
+                .forEach(key -> {
+                    sb.append(key.getKey()).append("\n");
+                });
+        return sb.toString();
+    }
+    public void addClientToMap(String name){
+        this.map.put(name, serverThread);
+    }
+    public Map<String, ServerThread> getMap(){
+        return this.map;
     }
 }
