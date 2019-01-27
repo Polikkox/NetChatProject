824185256e14e0ad16d7df0711e7bc10c1072a4b
Polikkox <damian.targosz11@gmail.com>
2019-01-27 18:57:01 +0100

    Add field address Add field to check if end address is correct Change time to more friendly view

--- src/main/java/com/codecool/networking/data/Message.java
+++ src/main/java/com/codecool/networking/data/Message.java
@@ -6,13 +6,22 @@ import java.time.LocalDateTime;
 public class Message implements Serializable{
     private String content;
     private String author;
+    private String address;
     private LocalDateTime createdAt;
+    private boolean isAddressCorrect = true;
 
     public Message(String content, String author) {
         this.content = content;
         this.author = author;
         this.createdAt = LocalDateTime.now();
     }
+    public Message(String content, String author, String address) {
+        this.content = content;
+        this.author = author;
+        this.address = address;
+        this.createdAt = LocalDateTime.now();
+    }
+    public Message(){}
 
     public String getContent() {
         return content;
@@ -28,6 +37,39 @@ public class Message implements Serializable{
 
     public void setAuthor(String author) {
         this.author = author;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCreatedAt() {
+        int year = createdAt.getYear();
+        int month = createdAt.getMonthValue();
+        int day = createdAt.getDayOfMonth();
+        int hour = createdAt.getHour();
+        int minute = createdAt.getMinute();
+        int second = createdAt.getSecond();
+        StringBuilder sb = new StringBuilder();
+        sb.append(year).append("-").append(month).append("-").append(day).append(" ")
+                .append(hour).append(":").append(minute).append(":").append(second);
+        return sb.toString();
+    }
+
+    public boolean isAddressCorrect() {
+        return isAddressCorrect;
+    }
+
+    public void setAddressCorrect(boolean addressCorrect) {
+        this.isAddressCorrect = addressCorrect;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 
     @Override
