2773622836eab624ce807139c3f4306382ea55d1
Polikkox <damian.targosz11@gmail.com>
2019-01-27 19:00:23 +0100

    Add serialization to sending sockets between users and server Refactor

--- src/main/java/com/codecool/networking/modes/Client.java
+++ src/main/java/com/codecool/networking/modes/Client.java
@@ -1,87 +1,73 @@
 package com.codecool.networking.modes;
 
-import com.codecool.networking.ScannerThread;
+import com.codecool.networking.handler.InputHandler;
+import com.codecool.networking.data.Message;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-    private Scanner scanner = new Scanner(System.in);
-    private ScannerThread scannerThread;
-    private boolean lock = true;
+    private Message message;
+    private InputHandler inputHandler;
+    private String name;
 
-    public void runClientSocket(String hostname, int portNumber) throws IOException {
+    public Client(){
+        this.inputHandler = new InputHandler();
+        this.name = getName();
+    }
 
+    public void runClientSocket(String hostname, int portNumber) throws IOException, ClassNotFoundException {
 
-        String name = getName();
-        PrintWriter outcomeMessage = null;
-        BufferedReader incomeMessage = null;
+        ObjectOutputStream outcomeMessage = null;
+        ObjectInputStream incomeMessage = null;
+
         try {
             Socket socket = new Socket(hostname, portNumber);
-            outcomeMessage = new PrintWriter(socket.getOutputStream(), true);
-            incomeMessage = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            incomeMessage = new ObjectInputStream(socket.getInputStream());
+            outcomeMessage = new ObjectOutputStream(socket.getOutputStream());
         }
         catch (IOException e) {
             e.printStackTrace();
         }
 
         sentNameOfClientToServer(outcomeMessage, name);
-        displayIncomeMessage(incomeMessage);
 
         while (true) {
-            String userInput = getUserInput();
-
-            if(userInput != null){
-                outcomeMessage.println(name + ": " + userInput);
-
+            if(incomeMessage.available() > 0){
+                incomeMessageHandler(incomeMessage);
             }
-            if(incomeMessage.ready()){
-                System.out.println(incomeMessage.readLine());
-            }
+            handleOutcomeMessage(outcomeMessage);
         }
     }
 
-    private void sentNameOfClientToServer(PrintWriter outcomeMessage, String name){
-        outcomeMessage.println(name);
+    private void handleOutcomeMessage(ObjectOutputStream outcomeMessage) throws IOException {
+        prepareMessage();
+
+        if(this.message != null){
+            message.setAuthor(name);
+            outcomeMessage.writeInt(0);
+            outcomeMessage.writeObject(message);
+            message = null;
+        }
     }
 
-    private void displayIncomeMessage(BufferedReader incomeMessage) throws IOException {
-        String inputMessage = incomeMessage.readLine();
-        System.out.println(inputMessage);
+    private void sentNameOfClientToServer(ObjectOutputStream outcomeMessage, String name) throws IOException {
+        outcomeMessage.writeObject(new Message("content", name));
     }
 
-    private boolean checkIfUserWantToDisconnect(String message) {
-        if (message == null || message.isEmpty()) {
-            return true;
-        }
-        return false;
+    private void incomeMessageHandler(ObjectInputStream incomeMessage) throws IOException, ClassNotFoundException {
+        incomeMessage.readInt();
+        Message inputMessage = (Message) incomeMessage.readObject();
+        System.out.println(inputMessage.getCreatedAt() + "\n" + inputMessage.getAuthor() + ": " + inputMessage.getContent());
     }
 
-    private String getUserInput() {
-
-        //Run only one thread waiting for user input
-        if(lock) {
-
-            scannerThread = new ScannerThread();
-            Thread thread = new Thread(scannerThread);
-            thread.start();
-            lock = false;
-        }
-        String input = scannerThread.getInput();
-
-        //Enable take another one input after got input
-        if(input != null){
-            scannerThread.setInput(null);
-            lock = true;
-        }
-        return input;
+    private void prepareMessage() {
+        this.message =  inputHandler.getInput();
     }
 
     private String getName() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello user enter your name: ");
         return scanner.nextLine();
     }
